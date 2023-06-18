617c8ae685330eb93a27e5d914fe5523013b4877
Kuba <dabrowskikuba.kd@gmail.com>
2023-06-18 18:00:42 +0200

    db isolation level REPEATABLE READ

--- server/routes/itemRoutes.js
+++ server/routes/itemRoutes.js
@@ -2,17 +2,24 @@ const router = require("express").Router()
 const path = require('path')
 const Item = require('../models/item.js')
 const fs = require('fs')
+const db = require('../db.js')
 
 router.post('/import', async (req, res) => {
     console.log(req.body.product)
     try {
-        const product = await Item.findOne({id: req.body.product}, '-_id')
+        //DB isolation level REPEATABLE READ
+        let session = db.startSession();
+        (await session).startTransaction({ readConcern: { level: 'snapshot' } });
+        const product = await Item.findOne({id: req.body.product}, '-_id');
+        (await session).commitTransaction();
+        (await session).endSession()
         if(product){
             res.status(201).send(product)
         } else {
             res.status(500).send({message: "Nie ma produktu"})
         }
     } catch (e) {
+        console.log(e)
         res.status(500).send({message: "Error!"})
     }
 })
@@ -20,6 +27,9 @@ router.post('/import', async (req, res) => {
 router.post('/export', async (req, res) => {
     console.log(req.body.product)
     try {
+        //DB isolation level REPEATABLE READ
+        let session = db.startSession();
+        (await session).startTransaction({ readConcern: { level: 'snapshot' } });
         const product = await Item.findOne({id: req.body.product.id})
         if(product){
             await Item.findOneAndUpdate({id: req.body.product.id})
@@ -28,6 +38,8 @@ router.post('/export', async (req, res) => {
             await new Item({...req.body.product}).save()
             res.status(201).send({message: "Dodano produkt"})
         }
+        (await session).commitTransaction();
+        (await session).endSession()
     } catch (e) {
         res.status(500).send({message: "Error!"})
     }
